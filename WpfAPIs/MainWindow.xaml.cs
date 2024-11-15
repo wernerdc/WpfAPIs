00e357dd48cc84a27a5cd9161fb9676c122d0b7e
Werner2M <Werner2M@C101PC-D6B71F>
2024-11-15 10:31:57 +0100

    Projektdateien hinzufügen.

---
+++ WpfAPIs/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAPIs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+         readonly WeatherControl _weatherControl = new WeatherControl();
+        readonly NewsControl _newsControl = new NewsControl();
+        readonly MoviesControl _moviesControl = new MoviesControl();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnWeather_Click(object sender, RoutedEventArgs e)
+        {
+            UserContentControl.Content = null;
+            UserContentControl.Content = _weatherControl;
+        }
+
+        private void BtnNews_Click(object sender, RoutedEventArgs e)
+        {
+            UserContentControl.Content = null;
+            UserContentControl.Content = _newsControl;
+        }
+
+        private void BtnFilm_Click(object sender, RoutedEventArgs e)
+        {
+            UserContentControl.Content = null;
+            UserContentControl.Content = _moviesControl;
+        }
+    }
+}
---
+++ WpfAPIs/Weather.xaml.cs
@@ -0,0 +1,50 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAPIs
+{
+    /// <summary>
+    /// Interaktionslogik für Weather.xaml
+    /// </summary>
+    public partial class WeatherControl : UserControl
+    {
+        public WeatherControl()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGetWeather_Click(object sender, RoutedEventArgs e)
+        {
+            await GetWeather();
+        }
+
+        private async Task GetWeather()
+        {
+            var client = new RestClient("https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?lat=35.5&lon=-78.5&units=imperial&lang=en");
+            var request = new RestRequest();
+            request.AddHeader("x-rapidapi-key", "9b8f8f04e6mshcaa3f16bf2623dcp162c0ajsne22516b5fcd4");
+            request.AddHeader("x-rapidapi-host", "weatherbit-v1-mashape.p.rapidapi.com");
+            //IRestResponse response = client.Execute(request);
+            var response = await client.GetAsync<WeatherModel>(request, CancellationToken.None);
+            if (response == null)
+                return;
+            dGridWeather.ItemsSource = null;
+            dGridWeather.ItemsSource = response.data;
+        }
+    }
+}
